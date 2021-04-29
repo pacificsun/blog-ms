f73b13606736d57722bab09e62d39947a65d0bd9
pacificsuraj <stsurajst@gmail.com>
2021-04-29 08:47:50 +0545

    till request problem with different microservicess

---
+++ src/PostList.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+
+export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+    console.log("res>>", res.data);
+    setPosts(res.data);
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // Object.values(posts) this returns array that have value of object( i.e posts)
+  const renderedPost = Object.values(posts).map((post) => {
+    return (
+      <div
+        key={post.id}
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPost}
+    </div>
+  );
+};
